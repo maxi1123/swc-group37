b53c2f778f0cd25be6b26be8b34b28b886f67eaf
Max Jonescu <jonescumaxi@gmail.com>
2020-10-11 14:27:36 +0200

    Add grid & edit Main

---
+++ SoftCon2020_Assignment_2/src/Grid.java
@@ -0,0 +1,9 @@
+public class Grid {
+    private static final int rows = 10;
+    private static final int columns = 10;
+
+    public static String[][] buildGrid(){
+        String grid[][] = new String[rows][columns];
+        return grid;
+    }
+}
--- SoftCon2020_Assignment_2/src/Main.java
+++ SoftCon2020_Assignment_2/src/Main.java
@@ -1,2 +1,32 @@
+import boats.*;
+
 public class Main {
+    public static void main(String[] args){
+        System.out.println("Welcome to Battleship!!");
+        Grid.buildGrid();
+        // Populate shipObjects with all the available ships
+        Ship[] shipObjects = new Ship[10];
+
+        int i;
+
+        for(i=0; i<10; i++){
+            if(i>5){
+                PatrolBoat patrolboat = new PatrolBoat();
+                shipObjects[i] = patrolboat;
+            }
+            else if(i>2){
+                Submarine submarine = new Submarine();
+                shipObjects[i] = submarine;
+            }
+            else if(i>0){
+                Battleship battleship = new Battleship();
+                shipObjects[i] = battleship;
+            }
+            else{
+                Carrier carrier = new Carrier();
+                shipObjects[i] = carrier;
+            }
+        }
+    }
+
 }
