1b6a9a934ca8f1a8364bb522dbf9a86cad79b5b5
arjvillan <villanthanam@gmail.com>
2020-10-11 17:23:51 +0200

    Merged validate into Main

--- SoftCon2020_Assignment_2/src/Main.java
+++ SoftCon2020_Assignment_2/src/Main.java
@@ -1,5 +1,7 @@
 import boats.*;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Welcome to Battleship!!");
@@ -71,4 +73,43 @@ public class Main {
 //        }
     }
 
+
+    public boolean InputValidate(Ship ship, String[][] ocean) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the position of your " + ship.getSize() + ":");
+        String Field1 = input.next();
+        String Field2 = input.next();
+        //working with ASCII values to find if gap between fields is valid for the ship length
+        if ((Field1.charAt(0) == Field2.charAt(0)) && (Field1.charAt(0) <= 'L') && (Field2.charAt(0) <= 'L')) {
+            if (Field2.charAt(1) - Field1.charAt(1) == ship.getSize()) {
+                for (int i = 0; i > ship.getSize(); i++) {
+                    if (ocean[Field1.charAt(0) - 'A'][i] != "") {
+                        System.out.println("The specified input is invalid");
+                        InputValidate(ship, ocean);
+                    } else {
+
+                    }
+                }
+            }
+        } else if ((Field1.charAt(1) == Field2.charAt(1)) && (Field1.charAt(1) <= '9') && (Field2.charAt(1) <= '9')) {
+            if (Field2.charAt(0) - Field1.charAt(0) == ship.getSize()) {
+                for (int i = 0; i > ship.getSize(); i++) {
+                    if (ocean[i][Field1.charAt(0) - 'A'] != "") {
+                        System.out.println("The specified input is invalid");
+                        InputValidate(ship, ocean);
+                    } else {
+
+                    }
+                }
+            } else {
+                System.out.println("The specified input is invalid");
+                InputValidate(ship, ocean);
+            }
+        }
+        //I hope this works
+        return false;
+    }
 }
+
+
+
