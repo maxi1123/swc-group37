9c9e3fb5374b82e8d47c43589ff597e3075d79a7
arjvillan <villanthanam@gmail.com>
2020-10-11 17:20:41 +0200

    Further work on Validate

--- SoftCon2020_Assignment_2/src/Validate.java
+++ SoftCon2020_Assignment_2/src/Validate.java
@@ -1,37 +1,41 @@
 import java.util.Scanner;
+import boats.*;
 
 public class Validate {
     Scanner input = new Scanner(System.in);
 
-    public boolean InputValidator(Ship ship, Ocean[][] ocean){
-        System.out.print("Please enter the position of your "+ship.name+":");
+    public boolean InputValidate(Ship ship, String[][] ocean) {
+        System.out.print("Please enter the position of your " + ship.getSize() + ":");
         String Field1 = input.next();
         String Field2 = input.next();
         //working with ASCII values to find if gap between fields is valid for the ship length
-        if ((Field1.charAt(0) == Field2.charAt(0))&&(Field1.charAt(0)<='L')&&(Field2.charAt(0)<='L')){
-            if (Field2.charAt(1)-Field1.charAt(1)==ship.length) {
-                for (int i=0;i>ship.length;i++){
-                    if (ocean[Field1.charAt(0)-'A'][i] != []){
+        if ((Field1.charAt(0) == Field2.charAt(0)) && (Field1.charAt(0) <= 'L') && (Field2.charAt(0) <= 'L')) {
+            if (Field2.charAt(1) - Field1.charAt(1) == ship.getSize()) {
+                for (int i = 0; i > ship.getSize(); i++) {
+                    if (ocean[Field1.charAt(0) - 'A'][i] != "") {
                         System.out.println("The specified input is invalid");
-                        return false;
-                    }
-                return true;
-            }
-            else{
-                System.out.println("The specified input is invalid");
-                return false;
-            }
-        }
-        else if (Field1.charAt(1)==Field2.charAt(1)){
-            if (Field2.charAt(0)-Field1.charAt(0)==ship.length){
+                        InputValidate(ship, ocean);
+                    } else {
 
+                    }
+                }
             }
-            else{
+        } else if ((Field1.charAt(1) == Field2.charAt(1)) && (Field1.charAt(1) <= '9') && (Field2.charAt(1) <= '9')) {
+            if (Field2.charAt(0) - Field1.charAt(0) == ship.getSize()) {
+                for (int i = 0; i > ship.getSize(); i++) {
+                    if (ocean[i][Field1.charAt(0) - 'A'] != "") {
+                        System.out.println("The specified input is invalid");
+                        InputValidate(ship, ocean);
+                    } else {
+
+                    }
+                }
+            } else {
                 System.out.println("The specified input is invalid");
-                return false;
+                InputValidate(ship, ocean);
             }
         }
+        //I hope this works
         return false;
     }
-    //I hope this works
 }
