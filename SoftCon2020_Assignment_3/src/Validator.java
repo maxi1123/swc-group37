a11a7a90075297f0820a9b48825f87e7c5048cc6
arjvillan <villanthanam@gmail.com>
2020-10-31 15:59:09 +0100

    fixed out of bounds in playflow attacking if/else statements

--- SoftCon2020_Assignment_3/src/Validator.java
+++ SoftCon2020_Assignment_3/src/Validator.java
@@ -53,8 +53,6 @@ public class Validator {
         Transformer t = new Transformer();
         int LetterPos = t.transformCoord(position.charAt(0));
         int NumberPos = position.charAt(1) - '0';
-        System.out.println(LetterPos);
-        System.out.println(NumberPos);
         if (position.length() == 2){
             if(LetterPos <= 9 && NumberPos <= 9){
                 if( AIGrid.grid[NumberPos][LetterPos] == "[ ]"){
