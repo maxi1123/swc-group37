8e1edd5403d6a746cca69fe9f6eb1513a654ec4d
mzehnde <max.zehnder@uzh.ch>
2020-11-04 16:07:09 +0100

    add stuff

---
+++ SoftCon2020_Assignment_4/src/customers/Creditcard.java
@@ -0,0 +1,17 @@
+package customers;
+
+public class Creditcard {
+    protected String name;
+    protected String surname;
+    protected int age;
+    protected int account_number;
+    protected int savings;
+    protected int ID;
+    protected int serial_number;
+    protected int security_code;
+    protected int expiration_date;
+    protected int limit;
+
+
+}
+
